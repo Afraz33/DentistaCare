03109ad1d6e968dd7ca5ab316c21a7a506cc6c90
Afraz33 <afraz3301@gmail.com>
2023-09-18 18:40:21 +0500

    Add Footer except map

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -5,6 +5,8 @@ import facebook from "../../assets/images/🦆 icon _facebook_.svg";
 import youtube from "../../assets/images/🦆 icon _youtube_.svg";
 import instagram from "../../assets/images/🦆 icon _instagram_.svg";
 import copyright from "../../assets/images/🦆 icon _copyright_.svg";
+import location from "../../assets/images/tabler_location.svg";
+import email from "../../assets/images/carbon_email.svg";
 function Footer() {
   return (
     <footer className={styles.footer}>
@@ -24,6 +26,45 @@ function Footer() {
                 <p className={styles.copyrightText}>
                   First stop Dental All Right Reserved
                 </p>
+              </div>
+            </div>
+            <div>
+              <h1 className={styles.usefulLinks}> Useful Links</h1>
+              <div className={styles.usefulLinksGrid}>
+                <div className={styles.gridColumn}>
+                  <h4 className={styles.gridText}>About Us</h4>
+                  <h4 className={styles.gridText}>Services</h4>
+                  <h4 className={styles.gridText}>FAQ</h4>
+                  <h4 className={styles.gridText}>Dentist</h4>
+                </div>
+                <div className={styles.gridColumn}>
+                  <h4 className={styles.gridText}>Help</h4>
+                  <h4 className={styles.gridText}>Privacy Policy</h4>
+                </div>
+              </div>
+            </div>
+            <div className={styles.contactInfo}>
+              <h1 className={styles.contact}>Contact</h1>
+              <p className={styles.number}>0000-1122778</p>
+              <p className={styles.number}>009988-2221</p>
+            </div>
+            <div className={styles.email_location}>
+              <div className={styles.inquiry}>
+                <img
+                  style={{ marginTop: "-1rem" }}
+                  src={email}
+                  alt="email"
+                ></img>
+                <h2 className={styles.inquiryText}>Inquiry</h2>
+              </div>
+              <p className={styles.email}>dentistacare@gmail.com</p>
+              <div className={styles.findUs}>
+                <img
+                  style={{ marginTop: "-0.5rem" }}
+                  src={location}
+                  alt="location"
+                ></img>
+                <h2 className={styles.inquiryText}>Find Us</h2>
               </div>
             </div>
           </div>
